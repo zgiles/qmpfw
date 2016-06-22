9777dcd3062c921fadae719b346b0caa8dd0beb4
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2016-06-22 18:32:05 +0200

    Add support for TL-WDR3500-v1 devices

--- targets.mk
+++ targets.mk
@@ -19,7 +19,7 @@
 # Any option defined in Makefile can be overrided from here, for instance
 #  override OWRT_SVN = svn://mysvn.com/owrt
 
-HW_AVAILABLE := ar71xx alfa-nx bullet kvm nsm2 nsm5 nsm5-xw nslm5-xw rocket-m-xw pico2 rocket rs rspro mc-mac1200r tl-2543 tl-703n tl-wr841n-v7 tl-wr841n-v8 tl-wr841n-v9 tl-842 tl-mr3020 tl-mr3040 tl-wa7510n tl-wdr3600 tl-wdr4300 tl-wdr4900-v1 wpe72 dragino2 alix soekris45xx x86 uap-pro unifi-ap vbox vmware vocore wt1520-4m wt1520-8m cf-wr800n dir-810l microwrt wrtnode wt3020-4m wt3020-8m xiaomi-miwifi-mini lamobo-r1 ar71xx-ib ath25-ib mpc85xx-ib ramips-ib x86-ib sunxi-ib
+HW_AVAILABLE := ar71xx alfa-nx bullet kvm nsm2 nsm5 nsm5-xw nslm5-xw rocket-m-xw pico2 rocket rs rspro mc-mac1200r tl-2543 tl-703n tl-wr841n-v7 tl-wr841n-v8 tl-wr841n-v9 tl-842 tl-mr3020 tl-mr3040 tl-wa7510n tl-wdr3500-v1 tl-wdr3600 tl-wdr4300 tl-wdr4900-v1 wpe72 dragino2 alix soekris45xx x86 uap-pro unifi-ap vbox vmware vocore wt1520-4m wt1520-8m cf-wr800n dir-810l microwrt wrtnode wt3020-4m wt3020-8m xiaomi-miwifi-mini lamobo-r1 ar71xx-ib ath25-ib mpc85xx-ib ramips-ib x86-ib sunxi-ib
 TBUILD_LIST := openwrt
 
 ifeq ($(T),ar71xx)
@@ -261,6 +261,15 @@ ifeq ($(T),tl-wa7510n)
   PROFILE:=ath-qmp-tiny-node
   IMAGE:=bin/$(ARCH)/openwrt-ar71xx-generic-tl-wa7510n-squashfs-factory.bin
   SYSUPGRADE:=bin/$(ARCH)/openwrt-ar71xx-generic-tl-wa7510n-squashfs-sysupgrade.bin
+endif
+
+ifeq ($(T),tl-wdr3500-v1)
+  NAME:=TP-Link_TL-WDR3500-v1
+  ARCH:=ar71xx
+  TBUILD:=openwrt
+  PROFILE:=ath-qmp-big-node
+  IMAGE:=bin/$(ARCH)/openwrt-ar71xx-generic-tl-wdr3500-v1-squashfs-factory.bin
+  SYSUPGRADE:=bin/$(ARCH)/openwrt-ar71xx-generic-tl-wdr3500-v1-squashfs-sysupgrade.bin
 endif
 
 ifeq ($(T),tl-wdr3600)
