a9d9e2c22807b7207ef10a2eda17350f2b35f867
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2016-06-22 18:32:05 +0200

    Add support for TL-WDR3500-v1 and TL-WA7510N devices

--- targets.mk
+++ targets.mk
@@ -20,7 +20,7 @@
 # Any option defined in Makefile can be overrided from here, for instance
 #  override OWRT_SVN = svn://mysvn.com/owrt
 
-HW_AVAILABLE := ar71xx alfa-nx bullet kvm nsm2 nsm5 nsm5-xw nslm5-xw rocket-m-xw pico2 rocket rs rspro mc-mac1200r tl-2543 tl-703n tl-wr841n-v7 tl-wr841n-v8 tl-wr841n-v9 tl-wr841n-v10 tl-842 tl-mr3020 tl-mr3040 tl-wdr3600 tl-wdr4300 tl-wdr4900-v1 wpe72 dragino2 alix soekris45xx x86 uap-pro unifi-ap vbox vmware vocore wt1520-4m wt1520-8m cf-wr800n dir-810l microwrt wrtnode wt3020-4m wt3020-8m miwifi-mini lamobo-r1 ar71xx-generic-ib ath25-generic-ib mpc85xx-generic-ib ramips-mt7620-ib ramips-rt305x-ib sunxi-generic-ib x86-generic-ib x86-geode-ib
+HW_AVAILABLE := ar71xx alfa-nx bullet kvm nsm2 nsm5 nsm5-xw nslm5-xw rocket-m-xw pico2 rocket rs rspro mc-mac1200r tl-2543 tl-703n tl-wr841n-v7 tl-wr841n-v8 tl-wr841n-v9 tl-wr841n-v10 tl-842 tl-mr3020 tl-mr3040 tl-wa7510n tl-wdr3500-v1 tl-wdr3600 tl-wdr4300 tl-wdr4900-v1 wpe72 dragino2 alix soekris45xx x86 uap-pro unifi-ap vbox vmware vocore wt1520-4m wt1520-8m cf-wr800n dir-810l microwrt wrtnode wt3020-4m wt3020-8m miwifi-mini lamobo-r1 ar71xx-generic-ib ath25-generic-ib mpc85xx-generic-ib ramips-mt7620-ib ramips-rt305x-ib sunxi-generic-ib x86-generic-ib x86-geode-ib
 TBUILD_LIST := lede
 
 ifeq ($(T),ar71xx)
@@ -289,6 +289,26 @@ ifeq ($(T),tl-mr3040)
   IMAGE:=bin/targets/$(ARCH)/$(SUBARCH)/qmp-ar71xx-generic-tl-mr3040-v1-squashfs-factory.bin
   SYSUPGRADE:=bin/targets/$(ARCH)/$(SUBARCH)/qmp-ar71xx-generic-tl-mr3040-v1-squashfs-sysupgrade.bin
 endif 
+
+ifeq ($(T),tl-wa7510n)
+  NAME:=TP-Link_TL-WA7510N
+  ARCH:=ar71xx
+  SUBARCH:=generic
+  TBUILD:=lede
+  PROFILE:=ath-qmp-tiny-node
+  IMAGE:=bin/$(ARCH)/$(SUBARCH)/qmp-ar71xx-generic-tl-wa7510n-squashfs-factory.bin
+  SYSUPGRADE:=bin/$(ARCH)/$(SUBARCH)/qmp-ar71xx-generic-tl-wa7510n-squashfs-sysupgrade.bin
+endif
+
+ifeq ($(T),tl-wdr3500-v1)
+  NAME:=TP-Link_TL-WDR3500-v1
+  ARCH:=ar71xx
+  SUBARCH:=generic
+  TBUILD:=lede
+  PROFILE:=ath-qmp-big-node
+  IMAGE:=bin/$(ARCH)/$(SUBARCH)/qmp-ar71xx-generic-tl-wdr3500-v1-squashfs-factory.bin
+  SYSUPGRADE:=bin/$(ARCH)/$(SUBARCH)/qmp-ar71xx-generic-tl-wdr3500-v1-squashfs-sysupgrade.bin
+endif
 
 ifeq ($(T),tl-wdr3600)
   NAME:=TP-Link_TL-WDR3600-v1
